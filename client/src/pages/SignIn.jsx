acff0b67a2eb6e6e606fc917da8682883487f0da
Vikat Rane <vikat9596@gmail.com>
2024-02-19 18:56:46 +0530

    Made the dashboard private

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -37,7 +37,7 @@ const SignIn = () => {
       
       if(res.ok){
         dispatch(signInSuccess(data));
-        navigate('/');
+        navigate('/dashboard');
       }
     } catch (error) {
         dispatch(signInFailure(error.message));
