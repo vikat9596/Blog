5243e5f211df6c8c2329e189470b3a9e96ec3ed3
Vikat Rane <vikat9596@gmail.com>
2024-03-11 05:09:20 +0530

    complete post page func

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,4 +1,4 @@
-import { parse } from "dotenv";
+
 import Post from "../models/post.model.js";
 import { errorHandler } from "../utils/error.js";
 
@@ -34,7 +34,7 @@ export const getposts = async (req, res, next) => {
         const posts = await Post.find({
             ...(req.query.userId && {userId: req.query.userId}),
             ...(req.query.category && {category: req.query.category}),
-            ...(req.query.slug && {category: req.query.slug}),
+            ...(req.query.slug && {slug: req.query.slug}),
             ...(req.query.postId && {_id: req.query.postId}),
             ...(req.query.searchTerm && {
                $or: [
@@ -64,7 +64,7 @@ export const getposts = async (req, res, next) => {
 
 
     } catch (error) {
-        
+        next(error);
     }
 };
 
@@ -81,20 +81,24 @@ export const deletePost = async (req, res, next) => {
 };
 
 export const updatePost = async (req, res, next) => {
-    if(!req.user.isAdmin || req.user.id !== req.params.userId) {
-        return next(errorHandler(403, 'You are not allowed to update a post'));
-    }
-    try {
-        const updatedPost = await Post.findByIdAndUpdate(req.params.postId, {
-            $set: {
-                title: req.body.title,
-                content: req.body.content,
-                category: req.body.category,
-                image: req.body.image,
-            },
-        }, {new: true});
-        res.status(200).json(updatedPost);
-    } catch (error) {
-        next(error);
-    }
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to update this post'));
+  }
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(
+      req.params.postId,
+      {
+        $set: {
+          title: req.body.title,
+          content: req.body.content,
+          category: req.body.category,
+          image: req.body.image,
+        },
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    next(error);
+  }
 };
