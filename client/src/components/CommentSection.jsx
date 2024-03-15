da13c7e8b7e36e893901e73c01583f09283c0e94
Vikat Rane <vikat9596@gmail.com>
2024-03-15 23:04:02 +0530

    add edit functionality to the comment component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -81,6 +81,14 @@ export default function CommentSection({postId}) {
           console.log(error.message);
         }
       };
+
+      const handleEdit = (comment, editedContent) => {
+        setComments(
+          comments.map((c) =>
+            c._id === comment._id ? {...c, content: editedContent} : c
+          )
+        );
+      };
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
         {currentUser ? 
@@ -131,6 +139,7 @@ export default function CommentSection({postId}) {
               key={comment._id}
               comment={comment}
               onLike={handleLike}
+              onEdit={handleEdit}
             />
           ))}
         </>
